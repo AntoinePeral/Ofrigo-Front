cf288e63717750ac41033e4b28d386749411dc99
0xMrik <agu.aymeric@hotmail.fr>
2023-04-14 23:31:06 +0200

    feat boutton toggle change recipes display

--- src/components/Main/Recipes/index.jsx
+++ src/components/Main/Recipes/index.jsx
@@ -1,47 +1,66 @@
 import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
+// Import des composants
 import Recipe from "../Recipe";
-import Toggle from "../Toggle";
 
+// Import des actions Redux
 import { toggleRecipesSource } from "../../../store/Recipes/action";
 
+// Import des composants MUI
 import Switch from "@mui/material/Switch";
 import FormGroup from "@mui/material/FormGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import { Container } from "@mui/material";
 
 const Recipes = () => {
-    const recipes = useSelector((state) => state.reducerRecipes.recipes);
-    const currentSource = useSelector((state) => state.reducerRecipes.source);
-    const dispatch = useDispatch();
-    const [isToggled, setIsToggled] = useState(false);
-  
-    const handleToggle = () => {
-        setIsToggled(!isToggled);
-        const newSource = currentSource === "testRecipesWithAllIngredients" ? "testRecipesWithoutAllIngredients" : "testRecipesWithAllIngredients";
-        dispatch(toggleRecipesSource(newSource));
-      };
-  
-      return (
-        <Container maxWidth="sm">
-          <FormGroup>
-            <FormControlLabel
-              control={
-                <Switch
-                  checked={isToggled}
-                  onChange={handleToggle}
-                  inputProps={{ "aria-label": "toggle source" }}
-                />
-              }
-              label={isToggled ? "On" : "Off"}
+  // Récupération des données depuis le store Redux
+  const recipes = useSelector((state) => state.reducerRecipes.recipes);
+  const currentSource = useSelector((state) => state.reducerRecipes.source);
+
+  // Initialisation du dispatch pour envoyer des actions au store Redux
+  const dispatch = useDispatch();
+
+  // Gestion de l'état local pour le switch
+  const [isToggled, setIsToggled] = useState(false);
+
+  // Gestionnaire de l'événement de basculement du switch
+  const handleToggle = () => {
+    // Mise à jour de l'état local pour le switch
+    setIsToggled(!isToggled);
+
+    // Détermination de la nouvelle source de recettes
+    const newSource =
+      currentSource === "testRecipesWithAllIngredients"
+        ? "testRecipesWithoutAllIngredients"
+        : "testRecipesWithAllIngredients";
+
+    // Envoi de l'action au store Redux pour mettre à jour la source
+    dispatch(toggleRecipesSource(newSource));
+  };
+
+  return (
+    <Container maxWidth="sm">
+      {/* Utilisation d'un FormGroup pour organiser le contrôle du switch */}
+      <FormGroup>
+        {/* Utilisation d'un FormControlLabel pour afficher le label à côté du switch */}
+        <FormControlLabel
+          control={
+            <Switch
+              checked={isToggled}
+              onChange={handleToggle}
+              inputProps={{ "aria-label": "toggle source" }}
             />
-          </FormGroup>
-          {recipes.map((recipe) => (
-            <Recipe key={recipe.id} recipe={recipe} />
-          ))}
-        </Container>
-      );
-    };
+          }
+          label={isToggled ? "On" : "Off"}
+        />
+      </FormGroup>
+      /* Affichage des recettes */
+      {recipes.map((recipe) => (
+        <Recipe key={recipe.id} recipe={recipe} />
+      ))}
+    </Container>
+  );
+};
 
 export default Recipes;
