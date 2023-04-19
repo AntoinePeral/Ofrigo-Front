8a274055739f58f2548da6da783b999d52dfa09a
0xMrik <agu.aymeric@hotmail.fr>
2023-04-19 09:58:23 +0200

    nothing happen

--- src/middleware/recipesMiddleware.jsx
+++ src/middleware/recipesMiddleware.jsx
@@ -5,9 +5,8 @@ const recipesMiddleware = (store) => (next) => (action) => {
   switch (action.type) {
     case FETCH_RECIPES:
       axios
-        .get("http://kevin-lienard-server.eddi.cloud/recipe")
+        .get("http://antoineperal-server.eddi.cloud/recipe")
         .then((response) => {
-          
           store.dispatch(saveRecipes(response.data));
         })
         .catch((err) => console.log(err));
